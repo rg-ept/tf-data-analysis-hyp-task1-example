28a156d96f0b68f79b097c32ef5538b65e3bc1e4
rg-ept <130046649+rg-ept@users.noreply.github.com>
2023-04-13 14:26:26 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,22 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 42877418
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha=0.02
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    
+    se = np.sqrt(p1 * (1 - p1) / x_cnt + p2 * (1 - p2) / y_cnt)
+    
+    t_stat = (p1 - p2) / se
+    dof = x_cnt + y_cnt - 2
+    t_crit = t.ppf(alpha / 2, dof)
+    
+    return abs(t_stat) > t_crit
+    
+   
